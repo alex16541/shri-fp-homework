8ee6735fdc51888d6f5433b46f170967d004b344
alex16541 <aleks.16541@yandex.ru>
2023-07-21 22:33:16 +0300

    init

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,83 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
+import { allPass, equals, compose, gt, gte, ifElse, length, match, not, prop, tap, andThen, pipe, tryCatch } from 'ramda';
+import Api from '../tools/api';
 
- const api = new Api();
+const api = new Api();
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+/**
+ * Я – пример, удали меня
+ */
+const wait = time => new Promise(resolve => {
+    setTimeout(resolve, time);
+})
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const toBinary = async (number) => await api.get('https://api.tech/numbers/base', {from: 10, to: 2, number});
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const getAnimal = async (id) => await api.get(`https://animals.tech/${id}`, {});
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+const square = (v) => Math.pow(v, 2); 
+const modThree = (v) => v % 3; 
+
+const isStringValid = allPass([
+    compose(
+        gte(10),
+        prop('length')
+    ),
+    compose(
+        not,
+        gt(2),
+        prop('length')
+    ),
+    compose(
+        not,
+        equals('-'),
+        prop('0'),
+        String
+    ),
+    compose(
+        equals(1),
+        length,
+        match(/^[\d.]+$/g)
+    )
+]);
+
+const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
+    const log = tap(writeLog);
+    const thenLog = andThen(log);
+
+    const preapareString = pipe(
+        Math.round,
+        log,
+        toBinary,
+        andThen(prop('result')),
+        andThen(prop('length')),
+        thenLog,
+        andThen(square),
+        thenLog,
+        andThen(modThree),
+        thenLog,
+        andThen(getAnimal),
+        andThen(prop('result')),
+        andThen(handleSuccess),
+    );
+
+    const process = compose(
+        ifElse(
+            isStringValid,
+            preapareString,
+            () => handleError('ValidationError'),
+        ),
+        log
+    );
+
+    tryCatch(
+        process,
+        () => handleError('NetworkError')
+    )(value);
+}
 
 export default processSequence;
--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -13,38 +13,138 @@
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
 
+import { props } from "ramda";
+import { all, allPass, compose, count, curry, equals, filter, gte, length, not, prop, reduce, tap } from "ramda";
+
 // 1. Красная звезда, зеленый квадрат, все остальные белые.
+const getTriangle = prop('triangle');
+const getStar = prop('star');
+const getCircle = prop('circle');
+const getSquare = prop('square');
+
+const isWhite = equals('white');
+const isRed = equals('red');
+const isGreen = equals('green');
+const isBlue = equals('blue');
+const isOrange = equals('orange');
+
+const isTriangeGreen = compose(
+    isGreen(),
+    getTriangle
+);
+
+const allFiguresColorIs = (isFn, figures) => compose(
+    all(isFn),
+    toList
+)(figures);
+
+const countOfWhite = count(isWhite);
+const countOfRed = count(isRed);
+const countOfGreen = count(isGreen);
+const countOfBlue = count(isBlue);
+const countOfOrange = count(isOrange);
+const countOfColors = reduce(
+    (colors, figure) => {
+        const newColors = {
+            [figure]: 0,
+            ...colors,
+        }
+
+        newColors[figure]++;
+        
+        return newColors
+    },
+    {}
+);
+
+const nEqualColorsExceptWhit = (n, figures) => compose(
+    Boolean,
+    length,
+    filter(x => x >= n),
+    toList,
+    countOfColors,
+    toList
+)(figures)
+
+const allEqual = (arr) => arr.every(v => v === arr[0]);
+
+const validateN6 = (colors) => {
+    return ( 
+        colors.green === 2 &&
+        colors.red === 1 && (
+            colors.blue === 1 ||
+            colors.white === 1 ||
+            colors.orange === 1
+        )
+    ); 
+};
+
+const toList = (object) =>  Object.values(object);
+
 export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
+    if (isWhite(triangle) || isWhite(circle)) {
         return false;
     }
 
-    return star === 'red' && square === 'green';
+    return isRed(star) && isGreen(square);
 };
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = (figures) => gte(countOfGreen(Object.values(figures)), 2);
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = (figures) => equals(countOfRed(toList(figures)), countOfBlue(toList(figures)));
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = ({star, square, triangle, circle}) => 
+        isBlue(circle) &&
+        isRed(star) &&
+        isOrange(square)
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = (figures) => nEqualColorsExceptWhit(3, figures);
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = (figures) => compose(
+    allPass([
+        compose(
+            validateN6,
+            countOfColors,
+            toList
+        ),
+        isTriangeGreen,
+    ]),
+)(figures);
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = (figures) => allFiguresColorIs(isOrange, figures);
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = (figures) => allPass([
+    compose(
+        not,
+        isRed,
+        getStar,
+    ),
+    compose(
+        not,
+        isWhite,
+        getStar,
+    )
+])(figures);
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = (figures) => allFiguresColorIs(isGreen, figures);
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = (figures) => allPass([
+    compose(
+        allEqual,
+        props(['triangle', 'square']),
+    ),
+    compose(
+        not,
+        isRed,
+        getTriangle,
+    )
+])(figures);
